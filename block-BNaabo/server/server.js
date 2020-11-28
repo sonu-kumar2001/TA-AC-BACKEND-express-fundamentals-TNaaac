fba5f67d26ec5cd6afad4eddf465841d6e3c0c72
sonu <sonurocckkerr@gmail.com>
2020-11-28 17:57:22 +0530

    completed block 4 of middlewares

---
+++ block-BNaabo/server/server.js
@@ -0,0 +1,26 @@
+let express = require('express');
+
+let app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.static(__dirname + "/public"))
+
+app.get('/',(req,res)=> {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post('/json',(req,res)=> {
+    console.log(req.body)
+})
+
+app.post('/contact',(req,res) => {
+    console.log(req.body);
+})
+
+
+app.listen(3000,() => {
+    console.log('server is running on port 3000');
+})
